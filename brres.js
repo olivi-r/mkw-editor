fff93614350bc07e02d1a8da1e93c3bb5e04a8dc
Olivia Ryan <olivia.r.dev@gmail.com>
2024-09-15 13:05:18 +0100

    Unabstract subfiles

--- brres.js
+++ brres.js
@@ -29,51 +29,56 @@ const RootIndexGroup = new Parser()
         .seek(16) // skip root node
         .array("files", {
           length: "length",
-          type: new Parser().nest({ type: IndexGroupEntry }).pointer("data", {
-            offset: function () {
-              return this.offset - this.$parent.brresOffset;
-            },
+          type: new Parser().nest({
+            type: new Parser().nest({ type: IndexGroupEntry }).pointer("data", {
+              offset: function () {
+                return this.offset - this.$parent.brresOffset;
+              },
+              formatter: function (item) {
+                return item.data;
+              },
+              type: new Parser().choice("data", {
+                tag: function () {
+                  switch (this.$parent.$parent.$parent.name) {
+                    case "3DModels(NW4R)":
+                      return 0;
+                    case "Textures(NW4R)":
+                      return 1;
+                    case "AnmTexSrt(NW4R)":
+                      return 2;
+                    case "AnmChr(NW4R)":
+                      return 3;
+                    case "AnmTexPat(NW4R)":
+                      return 4;
+                    case "AnmClr(NW4R)":
+                      return 5;
+                    case "AnmShp(NW4R)":
+                      return 6;
+                    case "AnmScn(NW4R)":
+                      return 7;
+                    case "Palettes(NW4R)":
+                      return 8;
+                    case "AnmVis(NW4R)":
+                      return 9;
+                  }
+                },
+                choices: {
+                  0: new Parser(),
+                  1: TEX0,
+                  2: new Parser(),
+                  3: new Parser(),
+                  4: new Parser(),
+                  5: new Parser(),
+                  6: new Parser(),
+                  7: new Parser(),
+                  8: PLT0,
+                  9: new Parser(),
+                },
+              }),
+            }),
             formatter: function (item) {
               return item.data;
             },
-            type: new Parser().choice("data", {
-              tag: function () {
-                switch (this.$parent.$parent.$parent.name) {
-                  case "3DModels(NW4R)":
-                    return 0;
-                  case "Textures(NW4R)":
-                    return 1;
-                  case "AnmTexSrt(NW4R)":
-                    return 2;
-                  case "AnmChr(NW4R)":
-                    return 3;
-                  case "AnmTexPat(NW4R)":
-                    return 4;
-                  case "AnmClr(NW4R)":
-                    return 5;
-                  case "AnmShp(NW4R)":
-                    return 6;
-                  case "AnmScn(NW4R)":
-                    return 7;
-                  case "Palettes(NW4R)":
-                    return 8;
-                  case "AnmVis(NW4R)":
-                    return 9;
-                }
-              },
-              choices: {
-                0: new Parser(),
-                1: TEX0,
-                2: new Parser(),
-                3: new Parser(),
-                4: new Parser(),
-                5: new Parser(),
-                6: new Parser(),
-                7: new Parser(),
-                8: PLT0,
-                9: new Parser(),
-              },
-            }),
           }),
         }),
     }),
