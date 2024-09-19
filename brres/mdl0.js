9ae17541523c0caa6e7bb8e24589c977cdf47608
Olivia Ryan <olivia.r.dev@gmail.com>
2024-09-19 02:11:52 +0100

    Add texture/palette links to mdl0

--- brres/mdl0.js
+++ brres/mdl0.js
@@ -220,6 +220,17 @@ const UVData = new Parser()
     },
   });
 
+const TexturePaletteLink = new Parser().nest({
+  type: new Parser()
+    .saveOffset("brresOffset", { formatter: (item) => -item })
+    .uint32("length")
+    .array("entries", {
+      length: "length",
+      type: new Parser().int32("materialOffset").int32("textureOffset"),
+    }),
+  formatter: (item) => item.entries,
+});
+
 export const MDL0 = new Parser()
   .useContextVars()
   .string("magic", { length: 4, assert: "MDL0" })
@@ -389,6 +400,62 @@ export const MDL0 = new Parser()
                 return this.offset - this.$parent.brresOffset;
               },
               type: UVData,
+            }),
+          }),
+        formatter: (item) => item.entries.map((e) => e.data),
+      }),
+    },
+  })
+  .choice({
+    tag: function () {
+      return this.textureLinkOffset === 0 ? 0 : 1;
+    },
+    choices: {
+      0: new Parser(),
+      1: new Parser().pointer("textureLinks", {
+        offset: function () {
+          return this.textureLinkOffset - this.brresOffset;
+        },
+        type: new Parser()
+          .saveOffset("brresOffset", { formatter: (item) => -item })
+          .seek(4)
+          .uint32("length")
+          .seek(16)
+          .array("entries", {
+            length: "length",
+            type: new Parser().nest({ type: IndexGroupEntry }).pointer("data", {
+              offset: function () {
+                return this.offset - this.$parent.brresOffset;
+              },
+              type: TexturePaletteLink,
+            }),
+          }),
+        formatter: (item) => item.entries.map((e) => e.data),
+      }),
+    },
+  })
+  .choice({
+    tag: function () {
+      return this.paletteLinkOffset === 0 ? 0 : 1;
+    },
+    choices: {
+      0: new Parser(),
+      1: new Parser().pointer("paletteLinks", {
+        offset: function () {
+          return this.paletteLinkOffset - this.brresOffset;
+        },
+        type: new Parser()
+          .saveOffset("brresOffset", { formatter: (item) => -item })
+          .seek(4)
+          .uint32("length")
+          .seek(16)
+          .array("entries", {
+            length: "length",
+            type: new Parser().nest({ type: IndexGroupEntry }).pointer("data", {
+              offset: function () {
+                return this.offset - this.$parent.brresOffset;
+              },
+              type: TexturePaletteLink,
             }),
           }),
         formatter: (item) => item.entries.map((e) => e.data),
