c71a28bc475ba7d48b0aabc62e58b782757892c7
Olivia Ryan <olivia.r.dev@gmail.com>
2024-09-19 19:34:13 +0100

    Add definition mdl0 subsection support

--- brres/mdl0.js
+++ brres/mdl0.js
@@ -1,6 +1,37 @@
 import { Parser } from "binary-parser";
 import { IndexGroupEntry, OffsetString } from "../util.js";
 
+const Definition = new Parser().nest({
+  type: new Parser().array("entries", {
+    type: new Parser().uint8("code").choice({
+      tag: "code",
+      choices: {
+        0: new Parser(),
+        1: new Parser(),
+        2: new Parser().uint16("boneIndex").uint16("parentMatrixIndex"),
+        3: new Parser()
+          .uint16("index")
+          .uint8("length")
+          .array("entries", {
+            length: "length",
+            type: new Parser().uint16("matrixIndex").floatbe("weight"),
+          }),
+        4: new Parser()
+          .uint16("materialIndex")
+          .uint16("objectIndex")
+          .uint16("boneIndex")
+          .uint8("priority"),
+        5: new Parser().uint16("matrixIndex").uint16("boneIndex"),
+      },
+    }),
+    readUntil: function (item, buffer) {
+      return item.code === 1;
+    },
+    formatter: (item) => item.slice(0, -1),
+  }),
+  formatter: (item) => item.entries,
+});
+
 const Bones = new Parser()
   .saveOffset("brresOffset", { formatter: (item) => -item })
   .seek(8)
@@ -430,6 +461,36 @@ export const MDL0 = new Parser()
       length: "length",
       type: new Parser().int32("index"),
     }),
+  })
+  .choice({
+    tag: function () {
+      return this.definitionOffset === 0 ? 0 : 1;
+    },
+    choices: {
+      0: new Parser(),
+      1: new Parser().pointer("definitions", {
+        offset: function () {
+          return this.definitionOffset - this.brresOffset;
+        },
+        type: new Parser()
+          .saveOffset("brresOffset", { formatter: (item) => -item })
+          .seek(4)
+          .uint32("length")
+          .seek(16)
+          .array("entries", {
+            length: "length",
+            type: new Parser()
+              .nest({ type: IndexGroupEntry })
+              .pointer("entries", {
+                offset: function () {
+                  return this.offset - this.$parent.brresOffset;
+                },
+                type: Definition,
+              }),
+          }),
+        formatter: (item) => item.entries,
+      }),
+    },
   })
   .choice({
     tag: function () {
