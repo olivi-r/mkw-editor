99243a122cefe2da20d59ddcd80580fb6a55a55e
Olivia Ryan <olivia.r.dev@gmail.com>
2024-09-20 01:21:39 +0100

    Add fur vector mdl0 subsection

--- brres/mdl0/furvector.js
+++ brres/mdl0/furvector.js
@@ -1,3 +1,21 @@
 import { Parser } from "binary-parser";
+import { OffsetString, Section } from "../../util.js";
 
-export const FurVectorSection = new Parser();
+export const FurVectorSection = Section(
+  "furVectorOffset",
+  "furVectors",
+  new Parser()
+    .saveOffset("brresOffset", { formatter: (item) => -item })
+    .seek(8)
+    .int32("offset")
+    .nest("name", { type: OffsetString })
+    .uint32("index")
+    .uint16("length")
+    .pointer("entries", {
+      offset: function () {
+        return this.offset - this.brresOffset;
+      },
+      type: new Parser().array("entries", { length: 3, type: "floatbe" }),
+      formatter: (item) => item.entries,
+    })
+);
